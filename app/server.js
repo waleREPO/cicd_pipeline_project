a5230e06c1666003a35f494f325e678e8e63d4b1
Adewale Adeyemi <walecxy619@gmail.com>
2026-09-08 22:48:56 -0400

    Update app message

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline!');
+  res.send('Hello from CI/CD Pipeline!  - V2');
 });
 
 app.get('/health', (req, res) => {
